5e3a923b2984c322bd7f572a3d8407774cc33888
Andrew Murray <darbyguitars@gmail.com>
2017-08-03 14:56:04 -0500

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 console.log('whats good bruh');
+
+
+let butnC = 0;
+let display = 0;
+let btn7 = document.getElementById('#seven');
+let btn8 = 0;
+let btn9 = 0;
+let btnDivivde = 0;
+let btn4 = 0;
+let btn5 = 0;
+let btn6 = 0;
+let btnMulti = 0;
+let btn1 = 0;
+let btn2 = 0;
+let btn3 = 0;
+let btnMinus = 0;
+let btn0 = 0;
+let btnDot = 0;
+let btnEqual = 0;
+let btnplus = 0;
+
+onClick
