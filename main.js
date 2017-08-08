4e42133fb676f27d46c16f1fbfabbf7ec75de750
Andrew Murray <darbyguitars@gmail.com>
2017-08-08 00:20:00 -0500

    now it works and looks good

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let ids = ['seven', 'eight', 'nine', 'divide', 'four', 'five', 'six', 'multi', '
 
 let btns = [btn7 = {}, btn8 = {}, btn9 = {}, btnDivivde = {}, btn4 = {}, btn5 = {}, btn6 = {}, btnMulti = {}, btn1 = {}, btn2 = {}, btn3 = {}, btnMinus = {}, btn0 = {}, btnDot = {}, btnPlus = {}]
 
+let btnC = {}
+let btnEquals = {}
+
+btnC.get = document.getElementById('clear');
+btnC.event = btnC.get.addEventListener('click', clearFn);
+btnEquals.get = document.getElementById('equal');
+btnEquals.event = btnEquals.get.addEventListener('click', equalFn);
+
 for (var i = 0; i < ids.length; i++) {
   let value = ids[i];
   btns[i].get = document.getElementById(value);
@@ -10,26 +18,13 @@ for (var i = 0; i < ids.length; i++) {
 
 function printFn() {
   document.querySelector('#display').textContent += event.path[0].textContent;
-  console.log(event);
-  console.log(document.getElementById('display').textContent)
 }
-
-let btnC = {}
-btnC.get = document.getElementById('clear');
-btnC.event = btnC.get.addEventListener('click', clearFn);
-
 
 function clearFn() {
-  document.querySelector('#display').textContent = ""
-  console.log(document.getElementById('display').textContent)
+  document.querySelector('#display').textContent = "";
 }
 
-let btnEquals = {}
-btnEquals.get = document.getElementById('equal');
-btnEquals.event = btnEquals.get.addEventListener('click', equalFn);
-
-
 function equalFn() {
-  let equalMath = document.getElementById('display').textContent
+  let equalMath = document.getElementById('display').textContent;
   document.querySelector('#display').textContent = eval(equalMath);
 }
