95343be24c4ddf103c8d76b384a9ff87e81357ed
Andrew Murray <darbyguitars@gmail.com>
2017-08-07 19:21:21 -0500

    more functionality

--- main.js
+++ main.js
@@ -1,63 +1,28 @@
-console.log('whats good bruh');
-
-// const btns = [
-//   btnC: document.getElementById('clear')],
-//   display: [document.getElementById('display')],
-//   btn7: [document.getElementById('seven'), '7'],
-//   btn8: [document.getElementById('eight'), '8'],
-//   btn9: [document.getElementById('nine'), '9'],
-//   btnDivivde: [document.getElementById('divide'), '/'],
-//   btn4: [document.getElementById('four'), '4'],
-//   btn5: [document.getElementById('five'), '5'],
-//   btn6: [document.getElementById('six'), '6'],
-//   btnMulti: [document.getElementById('multi'), '*'],
-//   btn1: [document.getElementById('one'), '1'],
-//   btn2: [document.getElementById('two'), '2'],
-//   btn3: [document.getElementById('three'), '3'],
-//   btnMinus: [document.getElementById('minus'), '-'],
-//   btn0: [document.getElementById('zero'), '0'],
-//   btnDot: [document.getElementById('dot'), '.'],
-//   btnEqual: [document.getElementById('equal'), '='],
-//   btnPlus: [document.getElementById('plus'), '+']
-// };
 
 
-// btns.e[0]addEventListener('click',prinction());
-//
-// function prinction(){
-//   document.querySelector('#display').innerHTML =  ;
-// }
+
 let ids = ['seven', 'eight', 'nine', 'divide', 'four', 'five', 'six', 'multi', 'one', 'two', 'three', 'minus', 'zero', 'dot', 'plus']
 
 let btns = [btn7 = {}, btn8 = {}, btn9 = {}, btnDivivde = {}, btn4 = {}, btn5 = {}, btn6 = {}, btnMulti = {}, btn1 = {}, btn2 = {}, btn3 = {}, btnMinus = {}, btn0 = {}, btnDot = {}, btnPlus = {}]
 
-
-
-console.log(ids.length);
-console.log(btns.length);
-
-
 for (var i = 0; i < ids.length; i++) {
   let value = ids[i];
-
-  let index =
-
   btns[i].get = document.getElementById(value);
-
-  btns[i].event = btns[i].get.addEventListener("click", printFn;
-
-  // console.log(btns[i].get.textContent)
-
-
-}
-function printFn() {
-  console.log(1)
-  document.querySelector('#display').innerHTML = btns.get.textContent;
+  btns[i].event = btns[i].get.addEventListener("click", printFn);
 }
 
+function printFn(){
+  document.querySelector('#display').textContent += event.path[0].textContent;
+  console.log(event);
+  console.log(document.getElementById('display').textContent)
+}
+
+let btnC = {}
+btnC.get= document.getElementById('clear');
+btnC.event = btnC.get.addEventListener('click', clearFn);
 
 
-
-
-
-console.log(btns);
+function clearFn(){
+document.querySelector('#display').textContent = ""
+console.log(document.getElementById('display').textContent)
+}
