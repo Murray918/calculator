a0e41e03a332c4e641f4712272e64a4876bab5c2
Andrew Murray <darbyguitars@gmail.com>
2017-08-10 16:33:45 -0500

    almost done with strobe but it does not work

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ let ids = ['seven', 'eight', 'nine', 'divide', 'four', 'five', 'six', 'multi', '
 let btns = [btn7 = {}, btn8 = {}, btn9 = {}, btnDivivde = {}, btn4 = {}, btn5 = {}, btn6 = {}, btnMulti = {}, btn1 = {}, btn2 = {}, btn3 = {}, btnMinus = {}, btn0 = {}, btnDot = {}, btnPlus = {}]
 
 let btnC = {}
+let delay = 0;
 let btnEquals = {}
+let bodyElement = document.querySelector(".calculatorWrapper");
+let requestAnimationFrame = window.requestAnimationFrame ||
+                            window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame ||
+                            window.msRequestAnimationFrame;
 
 btnC.get = document.getElementById('clear');
-btnC.event = btnC.get.addEventListener('click', clearFn);
+btnC.get.addEventListener('click', clearFn);
 btnEquals.get = document.getElementById('equal');
-btnEquals.event = btnEquals.get.addEventListener('click', equalFn);
+btnEquals.get.addEventListener('click', equalFn);
 
 for (var i = 0; i < ids.length; i++) {
   let value = ids[i];
@@ -16,15 +22,59 @@ for (var i = 0; i < ids.length; i++) {
   btns[i].event = btns[i].get.addEventListener("click", printFn);
 }
 
+let display = document.querySelector('#display');
+display.textContent = ''
+
 function printFn() {
-  document.querySelector('#display').textContent += event.path[0].textContent;
+  if (display.textContent === "IT'S LIT!") {
+    display.textContent = ''
+  }
+
+  if (display.textContent.length <= 10) {
+    display.textContent += event.path[0].textContent;
+  };
 }
 
 function clearFn() {
-  document.querySelector('#display').textContent = "";
+  document.querySelector('#display').textContent = "IT'S LIT!";
 }
 
 function equalFn() {
   let equalMath = document.getElementById('display').textContent;
-  document.querySelector('#display').textContent = eval(equalMath);
+  document.querySelector('#display').textContent = eval(equalMath).toFixed(6);
+}
+
+function changeColor() {
+    delay++;
+
+    if (delay > 4) {
+        bodyElement.style.backgroundColor = getRandomColor();
+        delay = 0;
+    }
+    requestAnimationFrame(changeColor);
+
+}
+
+function getRandomColor() {
+    // creating a random number between 0 and 255
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    // going from decimal to hex
+    let hexR = r.toString(16);
+    let hexG = g.toString(16);
+    let hexB = b.toString(16);
+    // making sure single character values are prepended with a "0"
+    if (hexR.length == 1) {
+        hexR = "0" + hexR;
+    }
+    if (hexG.length == 1) {
+        hexG = "0" + hexG;
+    }
+    if (hexB.length == 1) {
+        hexB = "0" + hexB;
+    }
+    // creating the hex value by concatenatening the string values
+    let hexColor = "#" + hexR + hexG + hexB;
+    return hexColor.toUpperCase();
 }
